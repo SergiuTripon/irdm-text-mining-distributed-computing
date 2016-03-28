9446b5dc2ac5eeaff941157b9cd075a55494280f
SergiuTripon <contact@sergiu-tripon.com>
2016-03-28 20:08:16 +0100

    Modified terrier scripts.

--- terrier-scripts/bm25_terrier.sh
+++ terrier-scripts/bm25_terrier.sh
@@ -6,31 +6,33 @@
 #    index_clueweb.sh
 #    bm25_terrier.sh
 
-# delete index folder if exists
+# delete results folder if exists
 rm -rf ../terrier-core-4.1/var/results/*
 
 echo >> ../terrier-core-4.1/etc/terrier.properties
-echo "# Question 2" >> ../terrier-core-4.1/etc/terrier.properties
+echo '# Question 2' >> ../terrier-core-4.1/etc/terrier.properties
 echo >> ../terrier-core-4.1/etc/terrier.properties
-echo "# path to topics file" >> ../terrier-core-4.1/etc/terrier.properties
+echo '# path to topics file' >> ../terrier-core-4.1/etc/terrier.properties
 echo trec.topics=../topics/trec2013-topics.txt >> ../terrier-core-4.1/etc/terrier.properties
-echo "# set topics parser" >> ../terrier-core-4.1/etc/terrier.properties
+echo '# set topics parser' >> ../terrier-core-4.1/etc/terrier.properties
 echo trec.topics.parser=TRECQuery >> ../terrier-core-4.1/etc/terrier.properties
-echo "# set query tags" >> ../terrier-core-4.1/etc/terrier.properties
+echo '# set query tags' >> ../terrier-core-4.1/etc/terrier.properties
 echo TrecQueryTags.doctag=TOP >> ../terrier-core-4.1/etc/terrier.properties
 echo TrecQueryTags.idtag=NUM >> ../terrier-core-4.1/etc/terrier.properties
 echo TrecQueryTags.process=TOP,NUM,TITLE >> ../terrier-core-4.1/etc/terrier.properties
 echo >> ../terrier-core-4.1/etc/terrier.properties
 
-echo "# set ranking model" >> ../terrier-core-4.1/etc/terrier.properties
+echo '# set ranking model' >> ../terrier-core-4.1/etc/terrier.properties
 echo trec.model=org.terrier.matching.models.BM25 >> ../terrier-core-4.1/etc/terrier.properties
-echo "print filename" >> ../terrier-core-4.1/etc/terrier.properties
+echo 'print filename' >> ../terrier-core-4.1/etc/terrier.properties
 echo trec.querying.outputformat.docno.meta.key=filename >> ../terrier-core-4.1/etc/terrier.properties
-echo "# path to results" >> ../terrier-core-4.1/etc/terrier.properties
+echo '# path to results' >> ../terrier-core-4.1/etc/terrier.properties
 echo trec.results=results >> ../terrier-core-4.1/etc/terrier.properties
 echo >> ../terrier-core-4.1/etc/terrier.properties
-echo "# path to qrels file" >> ../terrier-core-4.1/etc/terrier.properties
+echo '# path to qrels file' >> ../terrier-core-4.1/etc/terrier.properties
 echo trec.qrels=../qrels/qrels.adhoc.txt >> ../terrier-core-4.1/etc/terrier.properties
+echo >> ../terrier-core-4.1/etc/terrier.properties
+echo '### End of properties added by Sergiu Tripon ###' >> ../terrier-core-4.1/etc/terrier.properties
 
 # perform batch retrieval
 ../terrier-core-4.1/bin/trec_terrier.sh -r
--- terrier-scripts/index_clueweb.sh
+++ terrier-scripts/index_clueweb.sh
@@ -19,43 +19,45 @@ rm -rf ../terrier-core-4.1/var/index/*
 # generate collection
 sh ../terrier-core-4.1/bin/trec_setup.sh ../clueweb12/
 
-echo "# path to index folder" >> ../terrier-core-4.1/etc/terrier.properties
+echo '### Beginning of properties added by Sergiu Tripon ###' >> ../terrier-core-4.1/etc/terrier.properties
+echo >> ../terrier-core-4.1/etc/terrier.properties
+echo '# path to index folder' >> ../terrier-core-4.1/etc/terrier.properties
 echo terrier.index.path=index/ >> ../terrier-core-4.1/etc/terrier.properties
-echo "# path to etc folder" >> ../terrier-core-4.1/etc/terrier.properties
+echo '# path to etc folder' >> ../terrier-core-4.1/etc/terrier.properties
 echo terrier.etc=../terrier-core-4.1/etc/ >> ../terrier-core-4.1/etc/terrier.properties
-echo "# path to share folder" >> ../terrier-core-4.1/etc/terrier.properties
+echo '# path to share folder' >> ../terrier-core-4.1/etc/terrier.properties
 echo terrier.share=../terrier-core-4.1/share/ >> ../terrier-core-4.1/etc/terrier.properties
 echo >> ../terrier-core-4.1/etc/terrier.properties
-echo "# path to collection" >> ../terrier-core-4.1/etc/terrier.properties
+echo '# path to collection' >> ../terrier-core-4.1/etc/terrier.properties
 echo collection.spec=collection.spec >> ../terrier-core-4.1/etc/terrier.properties
-echo "# specify tags to index content from" >> ../terrier-core-4.1/etc/terrier.properties
+echo '# specify tags to index content from' >> ../terrier-core-4.1/etc/terrier.properties
 echo FieldTags.process=TITLE,H1,H2,H3,H4,H5,H6,ELSE >> ../terrier-core-4.1/etc/terrier.properties
 echo >> ../terrier-core-4.1/etc/terrier.properties
-echo "# collection type" >> ../terrier-core-4.1/etc/terrier.properties
+echo '# collection type' >> ../terrier-core-4.1/etc/terrier.properties
 echo trec.collection.class=SimpleFileCollection >> ../terrier-core-4.1/etc/terrier.properties
-echo "# document type" >> ../terrier-core-4.1/etc/terrier.properties
+echo '# document type' >> ../terrier-core-4.1/etc/terrier.properties
 echo indexing.simplefilecollection.extensionsparsers=txt:TaggedDocument >> ../terrier-core-4.1/etc/terrier.properties
 echo >> ../terrier-core-4.1/etc/terrier.properties
-echo "# words to be ignored" >> ../terrier-core-4.1/etc/terrier.properties
+echo '# words to be ignored' >> ../terrier-core-4.1/etc/terrier.properties
 echo stopwords.filename=stopword-list.txt >> ../terrier-core-4.1/etc/terrier.properties
-echo "# how documents need to be tokenized" >> ../terrier-core-4.1/etc/terrier.properties
+echo '# how documents need to be tokenized' >> ../terrier-core-4.1/etc/terrier.properties
 echo termpipelines=Stopwords,PorterStemmer >> ../terrier-core-4.1/etc/terrier.properties
 echo >> ../terrier-core-4.1/etc/terrier.properties
-echo "# index information other than document text" >> ../terrier-core-4.1/etc/terrier.properties
+echo '# index information other than document text' >> ../terrier-core-4.1/etc/terrier.properties
 echo indexer.meta.forward.keys=filename >> ../terrier-core-4.1/etc/terrier.properties
 echo indexer.meta.reverse.keys=filename >> ../terrier-core-4.1/etc/terrier.properties
-echo "# specify character length" >> ../terrier-core-4.1/etc/terrier.properties
+echo '# specify character length' >> ../terrier-core-4.1/etc/terrier.properties
 echo indexer.meta.forward.keylens=64 >> ../terrier-core-4.1/etc/terrier.properties
 echo >> ../terrier-core-4.1/etc/terrier.properties
-echo "# Question 1" >> ../terrier-core-4.1/etc/terrier.properties
+echo '# Question 1' >> ../terrier-core-4.1/etc/terrier.properties
 echo >> ../terrier-core-4.1/etc/terrier.properties
-echo "# convert document text to lower case" >> ../terrier-core-4.1/etc/terrier.properties
+echo '# convert document text to lower case' >> ../terrier-core-4.1/etc/terrier.properties
 echo lowercase=TRUE >> ../terrier-core-4.1/etc/terrier.properties
 echo >> ../terrier-core-4.1/etc/terrier.properties
-echo "# tokenize document text by splitting at all non-alphanumeric characters" >> ../terrier-core-4.1/etc/terrier.properties
+echo '# tokenize document text by splitting at all non-alphanumeric characters' >> ../terrier-core-4.1/etc/terrier.properties
 echo tokeniser=EnglishTokeniser >> ../terrier-core-4.1/etc/terrier.properties
 echo >> ../terrier-core-4.1/etc/terrier.properties
-echo "# set index name" >> ../terrier-core-4.1/etc/terrier.properties
+echo '# set index name' >> ../terrier-core-4.1/etc/terrier.properties
 echo terrier.index.prefix=terrier_clueweb_index >> ../terrier-core-4.1/etc/terrier.properties
 
 # generate index
